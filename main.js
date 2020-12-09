ca29a9e958e9d71459d82d749b6ce740625b16c7
Samuel McKean <samuel.mckean@gmail.com>
2020-12-08 21:50:07 -0600

    got giphy working

--- main.js
+++ main.js
@@ -11,27 +11,51 @@ function getCity() {
 
 // makes the API request to get the weather data
 function getWeather(city, units) {
-  fetch
+  return fetch
   (
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHERKEY}&units=${units}`, 
     { mode: 'cors' }
   )
     .then((response) => response.json())
-    .then((data) => {
-      const temp = document.createElement('p');
-      temp.id = 'temp';
-      temp.innerText = 'Temperature: ' + data.main.temp;
-      const description = document.createElement('p');
-      description.id = 'description';
-      description.innerText = 'Weather: ' + data.weather[0].description;
-      document.querySelector('#root').prepend(temp, description);
-      return data.main.temp;
+    .catch((error) => console.log(error));
+}
+
+function getGif(searchTerm) {
+  return fetch
+  (
+    `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHYKEY}&s=${searchTerm}`,
+    { mode: 'cors' }
+  )
+  .then((response) => response.json())
+  .then((gifData) => gifData.data.images.original.url)
+  .catch((error) => console.log(error));
+}
+
+function updateDOM(data) {
+  // start async process of getting gif
+  const gifUrl = getGif(data.weather[0].description)
+    .then((url) => {
+      // create img element for gif of weather
+      const img = document.createElement('img');
+      img.id = 'gif';
+      img.src = url;
+      document.querySelector('#root').prepend(img);
     })
     .catch((error) => console.log(error));
+  // create paragraph elements to display weather data
+  const temp = document.createElement('p');
+  temp.id = 'temp';
+  temp.innerText = 'Temperature: ' + data.main.temp;
+  const description = document.createElement('p');
+  description.id = 'description';
+  description.innerText = 'Weather: ' + data.weather[0].description;
+  document.querySelector('#root').prepend(temp, description);
 }
 
 document.querySelector('form').addEventListener('submit', function(event) {
   event.preventDefault();
   const city = getCity();
-  getWeather(city, units);
+  getWeather(city, units)
+    .then((weatherData) => updateDOM(weatherData))
+    .catch((error) => console.log(error));    
 })
